a686bf4604d9bbfd5a9848f4280f2f82f27db56f
Александр Самаркин <aysamarkin@sberbank.ru>
2025-04-30 13:37:03 +0300

    added new test and example if-else

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,45 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+  private Object object;
 
-        cycle(4);
-    }
-        // метод cycle_for
-        public static void cycle(int count){
-        for (int i = 1; i <= count; i++) {
+  public static void main(String[] args) {
+    // Press Alt+Enter with your caret at the highlighted text to see how
+    // IntelliJ IDEA suggests fixing it.
+    System.out.println("Hello and welcome!");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    cycle(10);
+  }
+
+  // метод cycle_for
+  public static List<Integer> cycle(int count) {
+    List<Integer> listNumbers = new ArrayList(count);
+
+    for (int i = 0; i < count; i++) {
+
+      // Press Shift+F9 to start debugging your code. We have set one breakpoint
+      // for you, but you can always add more by pressing Ctrl+F8.
+//      System.out.println("i = " + i);
+      listNumbers.add(i);
+//      if (i % 2 == 0) {
+//        System.out.println("even");
+//      } else if (i % 2 == 1) {
+//        System.out.println("not-even");
+//      } else {
+//        System.out.println("odd");
+//      }
+      String result = (i % 2 == 0) ? "even" : "not-even";
+      System.out.println(result);
     }
+    System.out.println("listNumbers = " + listNumbers);
+
+    return listNumbers;
+  }
 }
--- src/test/java/org/example/TestMain1.java
+++ src/test/java/org/example/TestMain1.java
@@ -1,11 +1,22 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMain1 {
+
+    @Test
+    public void testCycle() {
+        List<Integer> expectedList = Arrays.asList(0, 1, 2, 3, 4, 5, 6);
+        List<Integer> actualList = Main.cycle(7);
+        assertEquals(expectedList, actualList, "cycle() должен возвращать числа от 1 до 7");
+    }
 
     @Test
     public void testCycleOutput() {
